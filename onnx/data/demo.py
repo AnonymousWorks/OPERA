fff1babef4abb8aba562aa3969bdd84f02998de5
AnonymousWorks <qingchaoshen@163.com>
2023-10-20 23:31:52 +0800

    add support for openvino_onnx

---
+++ onnx/data/demo.py
@@ -0,0 +1,3 @@
+make_graph(op_type='Expand', kwargs={}, input_name=('X', 'shape'), input_shape=([1, 3, 1], [2]), input_dtype=('FLOAT', 'INT64'), output_name=('Y',), output_shape=([1, 3, 1],), output_dtype=('FLOAT',))
+make_graph(op_type='Expand', kwargs={}, input_name=('X', 'shape'), input_shape=([1, 3, 1], [2]), input_dtype=('FLOAT', 'INT64'), output_name=('Y',), output_shape=([1, 3, 3],), output_dtype=('FLOAT',))
+make_graph(op_type='Expand', kwargs={}, input_name=('X', 'shape'), input_shape=([1, 3, 1], [3]), input_dtype=('FLOAT', 'INT64'), output_name=('Y',), output_shape=([3, 3, 3],), output_dtype=('FLOAT',))
